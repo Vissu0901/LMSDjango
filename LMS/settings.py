ae2b4226ef7d574416936bb10b2480f9b4ffecf3
Vissu0901 <viswanath.varikuti09@gmail.com>
2022-07-15 13:08:57 +0530

    commit

--- LMS/settings.py
+++ LMS/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jv-2voxd9wi#*30ruh(0_vkhoo(pu5&&2@njw2kb72_3sw0dv&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
